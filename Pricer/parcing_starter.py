f646bcf8dbe556e59e434e1a5b59291fa92ebd4d
Feel-Coins <99021673+Feel-Coins@users.noreply.github.com>
2023-02-13 17:07:49 +0300

    Delete Pricer directory

--- Pricer/parcing_starter.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.startfile(r'C:\Users\79134\Desktop\python\Pricer\onlinetrade\parsing_onlinetrade.py')
-os.startfile(r'C:\Users\79134\Desktop\python\Pricer\Citilink\parsing_citilink.py')
