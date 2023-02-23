640d0b20a2f6c08fa20b6b74faec5721b6795b14
Feel-Coins <feel.coins@yandex.ru>
2023-02-23 14:30:22 +0300

    Compile main pages, add templates and styles.

--- Django/unicorn/feed/views.py
+++ Django/unicorn/feed/views.py
@@ -2,58 +2,64 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    username = 'choom'
-    if 'username' in request.COOKIES:
-        username = request.COOKIES['username']
-        element = "http://127.0.0.1:8000/profile"
-        el_name = 'My profile'
-    else:
-        element = "http://127.0.0.1:8000/reg"
-        el_name = 'Registration'
-    return render(request, 'Unicorn.html', context={"username": username, "element": element, "el_name": el_name})
-
-
-def about(request):
-    return HttpResponse('<h1>About unicorn:</h1>'
-                        'The Unicorn is a site with AI rendered art, that can be purchased')
-
-
-def contact(request):
-    return HttpResponse('<h1>Contact us:</h1>'
-                        'Email: vladislav4ik.sib@gmail.com')
-
-
-def profile(request):
-    user = {'username': 'choom', 'age': 'unknown'}
-    if 'username' and 'age' in request.COOKIES:
-        user = {'username': request.COOKIES['username'], 'age': request.COOKIES['age']}
-    return render(request, 'Profile.html', context=user)
-
-
-
 def data(request):
     host = request.META['HTTP_HOST']
     user_agent = request.META['HTTP_USER_AGENT']
     return HttpResponse(f"""
     <p>Host: {host}</p>
-    <p>Path: {path}</p>
     <p>User-agent: {user_agent}</p>
     <br>
     {request.META}
     """)
 
 
+def context(request):
+    username = 'choom'
+    age = 'unknown'
+    cities = 'no location'
+    if 'username' in request.COOKIES:
+        username = request.COOKIES['username']
+        age = request.COOKIES['age']
+        cities = request.COOKIES['cities']
+        element = "http://127.0.0.1:8000/profile"
+        el_name = 'My profile'
+    else:
+        element = "http://127.0.0.1:8000/reg"
+        el_name = 'Registration'
+    return {"username": username, "element": element, "el_name": el_name, "age": age, "cities": cities}
+
+
+def index(request):
+    return render(request, 'unicorn.html', context=context(request))
+
+
+def about(request):
+
+    return render(request, 'about.html', context=context(request))
+
+
+def contact(request):
+    return render(request, 'contacts.html', context=context(request))
+
+
+def registration(request):
+    return render(request, 'reg.html')
+
+
+def profile(request):
+    return render(request, 'profile.html', context=context(request))
+
+
 def catalog(request):
-    return HttpResponse('Your shoplist')
+    return render(request, 'catalog.html', context=context(request))
 
 
 def new(request):
-    return HttpResponse('New products')
+    return render(request, 'new_prods.html', context=context(request))
 
 
 def top(request):
-    return HttpResponse('Top products')
+    return render(request, 'top_prods.html', context=context(request))
 
 
 def product(request, id):
@@ -71,18 +77,20 @@ def questions(request, id):
 def set_cookies(request):
     username = request.GET.get('username', 'choom')
     age = request.GET.get('age', 'unknown')
-    response = HttpResponse(f'''<p1>Welcome {username}, your age is {age}!</p1>
+    cities = request.GET.getlist('cities', 'no location')
+    response = HttpResponse(f'''<p1>Welcome {username}, your age is {age} and your cities is {cities}</p1>
                             <br>
                             <a href="http://127.0.0.1:8000">back home</a>''')
     response.set_cookie('username', username)
     response.set_cookie('age', age)
+    response.set_cookie('cities', cities)
     return response
 
 
 def get_cookies(request):
     username = request.COOKIES['username']
-    return HttpResponse(f'You are {username}!')
-
-
-def registration(request):
-    return render(request, 'reg.html')
+    age = request.COOKIES['age']
+    cities = request.COOKIES['cities']
+    return HttpResponse(f'You are {username}!'
+                        f'Your age is {age}'
+                        f'Your cities is {cities}')
